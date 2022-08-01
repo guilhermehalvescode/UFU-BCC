0138f311e25690a9df4d09513b14daaadda4ce9e
guilherme.092011 <guilherme.092011@hotmail.com>
2022-08-01 15:32:46 -0300

    aa: mwis fix

--- 5p/aa/algorithms/mwis.js
+++ 5p/aa/algorithms/mwis.js
@@ -1,10 +1,11 @@
 function mwisStub(v, pos) {
   if (v.length - pos <= 2) return v[pos] ?? 0;
 
-  if (v.length - pos === 3) return v[pos] + v[pos + 3];
+  if (v.length - pos === 3) return v[pos] + v[pos + 2];
 
-  const sol1 = mwis(v, pos + 1);
-  const sol2 = mwis(v, pos + 2);
+  const sol1 = mwisStub(v, pos + 2);
+  const sol2 = mwisStub(v, pos + 3);
+
   return v[pos] + Math.max(sol1, sol2);
 }
 
