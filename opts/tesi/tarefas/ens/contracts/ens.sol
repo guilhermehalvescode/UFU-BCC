eb45f066fd0fcef366afe9bdb108920a548d0bba
guilhermealvesc <guilherme.092011@hotmail.com>
2023-09-11 05:41:30 -0300

    tesi: prettier dev modules

--- opts/tesi/tarefas/ens/contracts/ens.sol
+++ opts/tesi/tarefas/ens/contracts/ens.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 contract ens {
     address owner;
     mapping(string => mapping(string => address)) public enses;
-		mapping(string => mapping(address => string)) public addressesSubdomains;
+    mapping(string => mapping(address => string)) public addressesSubdomains;
 
     constructor() {
         owner = msg.sender;
@@ -15,34 +15,36 @@ contract ens {
         _;
     }
 
-
     // registra um "dominio" para msg.sender
     // registerDomain(string newDomain) onlyOwner;
     function registerDomain(string memory newDomain) public onlyOwner {
-				string memory addr = string(abi.encodePacked(msg.sender));
+        string memory addr = string(abi.encodePacked(msg.sender));
         enses[newDomain][addr] = msg.sender;
     }
 
-
     // O msg.sender define um par chave-valor em domain
     // setValue("TOPICOS","ivan") faz ENS["TOPICOS"]["ivan"] = msg.sender
-    // setValue(string domain, string v) 
-		// OBS: should domain owner register the subdomains? this way anyone can register a subdomain
+    // setValue(string domain, string v)
+    // OBS: should domain owner register the subdomains? this way anyone can register a subdomain
     function setValue(string memory domain, string memory v) public {
         enses[domain][v] = msg.sender;
-				addressesSubdomains[domain][msg.sender] = v;
+        addressesSubdomains[domain][msg.sender] = v;
     }
 
-
-		// getValue("TOPICOS","ivan") retorna  o endereço ADDRESS  quando ENS["TOPICOS"]["ivan"] = ADDRESS
-		// getValue(string domain, string v) returns (address)
-    function getValue(string memory domain, string memory v) public view returns (address) {
+    // getValue("TOPICOS","ivan") retorna  o endereço ADDRESS  quando ENS["TOPICOS"]["ivan"] = ADDRESS
+    // getValue(string domain, string v) returns (address)
+    function getValue(
+        string memory domain,
+        string memory v
+    ) public view returns (address) {
         return enses[domain][v];
     }
 
-		// getValue("TOPICOS") retorna  a string RET quando ENS["TOPICOS"][RET] = msg.sender
-		// getValue(string domain) returns (string)
-    function getValue(string memory domain) public view returns (string memory) {
+    // getValue("TOPICOS") retorna  a string RET quando ENS["TOPICOS"][RET] = msg.sender
+    // getValue(string domain) returns (string)
+    function getValue(
+        string memory domain
+    ) public view returns (string memory) {
         return addressesSubdomains[domain][msg.sender];
     }
 }
