d8bdb0f2bc9f9e137f5e84748cc31f7841d0fa72
guilhermealvesc <guilherme.092011@hotmail.com>
2023-09-26 14:10:05 -0300

    tesi: init tarefa coin flipping

---
+++ opts/tesi/tarefas/coinf/Contracts/BonusToken.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.6.0;
+
+/**
+  @author Ivan da Silva Sendin
+  @title BonusToken
+  
+  This contract aims to manage the extra credits of this course.
+  
+*/
+
+import "OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/token/ERC20/ERC20.sol";
+
+contract BonusToken is ERC20 {
+    mapping(address => bool) trustedAddresses;
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sorry!");
+        _;
+    }
+
+    modifier onlyTrustedAddresses(address a) {
+        require(
+            trustedAddresses[msg.sender] || trustedAddresses[a],
+            "This transaction is not allowed!"
+        );
+        _;
+    }
+
+    constructor(uint256 initialSupply) public ERC20("BonusToken", "BTK") {
+        owner = msg.sender;
+        _mint(msg.sender, initialSupply);
+    }
+
+    function addAddress(address a) public onlyOwner {
+        trustedAddresses[a] = true;
+    }
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) public override onlyTrustedAddresses(recipient) returns (bool) {
+        return super.transfer(recipient, amount);
+    }
+
+    function getExtraCredit(address student) public view returns (uint256) {
+        uint256 b = balanceOf(student);
+        if (b < 10000) {
+            return 0;
+        }
+        uint256 extra = (b - 10000) / 1000;
+        if (extra > 20) {
+            return 20;
+        }
+        return extra;
+    }
+}
